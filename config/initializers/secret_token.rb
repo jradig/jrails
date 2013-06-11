bd09925b6c4dbf0a0c45c6a06837b33d913d9143
Johannes Radig <jradig@gmail.com>
2013-06-11 22:17:34 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jrails::Application.config.secret_token = '993d73d898f1583613b7a20d54d6b4df82b64367ee742a08a7d772df57b1fa53976ecb4be64f743a05c8f202a3ea4d33095932a0e18262a9bf885ba087f2b2c8'
